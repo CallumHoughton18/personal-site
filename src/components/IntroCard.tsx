32c6960463a8aa4852bd08eb190231ac4fbcd69c
CallumHoughton18 <Callum.Houghton13@hotmail.co.uk>
2020-11-27 16:53:37 +0000

    feat(introcard.tsx): made intro card larger, with better styled text

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -8,9 +8,12 @@ const IntroCard = (props: IntroCardProps): JSX.Element => {
   return (
     <div className="flex items-center justify-center m-8 rounded-lg border border-gray-800 shadow-l bg-gray-700 h-32 lg:max-w-4xl">
       <div className="p-10">
-        <span className="text-gray-200 sm:text-lg xl:text-xl font-bold">
-          Hello, My Name is Callum and I am a Software Developer
-        </span>
+        <h2 className="text-gray-400 text-center sm:text-xl xl:text-2xl">
+          Callum Houghton
+        </h2>
+        <h1 className="text-gray-100 text-center sm:text-4xl xl:text-8xl font-extrabold tracking-wide">
+          Web / Mobile Developer
+        </h1>
       </div>
     </div>
   );
