d0af64878b492f4775bd526de6c47aa7df5b7f13
CallumHoughton18 <Callum.Houghton13@hotmail.co.uk>
2020-12-04 16:32:34 +0000

    feat(list.tsx skills.tsx): style adjustments
    make skills header tracking wider, add bottom margin to skills list items

--- src/components/layout/List.tsx
+++ src/components/layout/List.tsx
@@ -6,7 +6,11 @@ type ListProps = {
 
 const List = ({ items }: ListProps): JSX.Element => {
   const listItems = items.map((item, indx) => {
-    return <li key={`${item}-${indx}`}>{item}</li>;
+    return (
+      <li key={`${item}-${indx}`} className="mb-1">
+        {item}
+      </li>
+    );
   });
   return <ul className="text-xl list-disc mb-5 md:mb-0">{listItems}</ul>;
 };
--- src/components/page components/index/Skills.tsx
+++ src/components/page components/index/Skills.tsx
@@ -4,7 +4,9 @@ import React from "react";
 const Skills = (): JSX.Element => {
   return (
     <div className="text-gray-100">
-      <h2 className=" text-center text-4xl font-bold">Experience In:</h2>
+      <h2 className=" text-center text-4xl font-bold tracking-wide">
+        Experience In:
+      </h2>
       <div className="flex flex-wrap justify-evenly md:content-center mt-20 md:flex-row flex-col">
         <List items={["C#", "TypeScript", "Python"]} />
         <List items={["Xamarin Forms", "React", "Flask"]} />
