36648e2f955fd0108671a5872cac8af81b43dc1f
CallumHoughton18 <callum.houghton13@hotmail.co.uk>
2021-04-15 16:19:40 +0100

    feat: fix sizing of UnderConstruction component

--- src/components/multipage components/UnderConstruction.tsx
+++ src/components/multipage components/UnderConstruction.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const UnderConstruction = (): JSX.Element => {
   return (
     <div className="flex flex-col items-center justify-center mt-36 mb-36">
-      <FaTools size="10x" className="text-snowStorm mb-5" />
+      <FaTools size="250" className="text-snowStorm mb-5" />
       <h1 className="tracking-widest text-snowStorm text-xl">
         Coming Soon (maybe)!
       </h1>
